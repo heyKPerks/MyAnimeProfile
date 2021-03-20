5eed9945f12ee8de1f521f1cd96b6edd98c01dc4
Kyle <gotkperks@gmail.com>
2021-03-19 23:03:00 -0400

    fixed show more character margins

--- assets/scripts/show-more-characters.js
+++ assets/scripts/show-more-characters.js
@@ -4,10 +4,13 @@ button on click, display more female div, hide button
 function more_female_btn() {
   var i;
   var characters = document.getElementsByClassName("more-female");
+  var container = document.getElementById("more_characters_fm");
   var femalebtn = document.getElementById("more_female_btn");
+  
 
   for (i = 0; i < characters.length; i++) {
     characters[i].style.display = "block";
   } 
+  container.style.marginTop = "10px";
   femalebtn.style.display = "none";
 }
