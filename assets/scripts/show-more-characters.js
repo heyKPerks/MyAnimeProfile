17e8bb60a1b1e597ad921e980edc59194ae37dab
Kyle <gotkperks@gmail.com>
2021-03-19 23:27:25 -0400

    created show more by genre button

--- assets/scripts/show-more-characters.js
+++ assets/scripts/show-more-characters.js
@@ -1,3 +1,15 @@
+/*Show more anime series by genre
+button on click, display scrolling_genre_cards, hide button
+*/
+function more_bygenre_btn() {
+  var container = document.getElementById("scrolling_genre_cards");
+  var button = document.getElementById("more_bygenre_btn"); 
+  
+  container.style.display = "flex";
+  container.style.marginTop = "1rem";
+  button.style.display = "none";
+}
+
 /*Show more female characters
 button on click, display more female div, hide button
 */
